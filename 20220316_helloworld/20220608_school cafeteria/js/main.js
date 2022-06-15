1f6919eaa346f6b40ccc6c8d8cc10b9677408c9b
hywn <w2106@e-mirim.hs.kr>
2022-06-15 11:47:37 +0900

    js toggle

--- 20220316_helloworld/20220608_school cafeteria/js/main.js
+++ 20220316_helloworld/20220608_school cafeteria/js/main.js
@@ -1,18 +1,19 @@
 // show/hide menu: toggle menu
-function toggleMenu(toggleId, navListId) {
+const toggleMenu = function (toggleId, navListId) {
   //html -> js
   const toggle = document.getElementById(toggleId);
   const navList = document.getElementById(navListId);
 
-  function clickHandler() {
+  const clickHandler = function () {
+    //show/hide menu: .show-menu
     navList.classList.toggle("show-menu");
-  }
+  };
 
   if (toggle && navList) {
     //toggle click
     toggle.addEventListener("click", clickHandler);
   }
-}
+};
 toggleMenu("nav-toggle", "nav-list");
 
 // const say = () => console.log("hello world4");
